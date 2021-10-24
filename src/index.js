59ca691d8e4e04b4432ff31eb8df232ce96f50cf
Oscar Molinar <oscarmolinar96@gmail.com>
2021-10-23 20:34:27 -0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { TodoList } from './components/TodoList';
-
-ReactDOM.render(
-  <TodoList />,
-  document.getElementById('root')
-);
